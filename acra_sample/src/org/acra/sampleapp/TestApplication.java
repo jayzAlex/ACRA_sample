7d98dc8bd341c2e3604fc1f8e77d0944c6edd01c
zhengja <zhengjinang@gmail>
2015-04-23 14:20:50 +0800

    version 1.0

--- acra_sample/src/org/acra/sampleapp/TestApplication.java
+++ acra_sample/src/org/acra/sampleapp/TestApplication.java
@@ -14,18 +14,17 @@ import android.os.Environment;
 	customReportContent = { ReportField.USER_CRASH_DATE,ReportField.ANDROID_VERSION, 
 	ReportField.PHONE_MODEL, ReportField.BRAND, ReportField.PRODUCT, ReportField.DEVICE_ID,
 	ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.STACK_TRACE,
-	ReportField.PACKAGE_NAME,ReportField.TOTAL_MEM_SIZE,ReportField.AVAILABLE_MEM_SIZE
+	ReportField.PACKAGE_NAME,ReportField.TOTAL_MEM_SIZE,ReportField.AVAILABLE_MEM_SIZE}
 //// 1.Sending reports to your own self-hosted script
 //formUriBasicAuthLogin = "user", 
 //formUriBasicAuthPassword = "password", 
 //formUri = "http://192.168.1.114:8080/TestAcra3/CrashServerlet",
 //httpMethod = org.acra.sender.HttpSender.Method.POST
-	
+//	
 //// 2.Sending reports by email
 //mailTo = "zhengja13438@hundsun.com",
 //mode = ReportingInteractionMode.TOAST,
 //resToastText = R.string.crash_toast_text
-	}
 )
 
 public class TestApplication extends Application {
